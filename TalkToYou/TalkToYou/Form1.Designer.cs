ee3c35b849e5b1d641cbca497432d6c0d6e16877
Yujie Yang <411942104@qq.com>
2018-02-04 23:52:43 +0800

    提交：客户端代码

---
+++ TalkToYou/TalkToYou/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TalkToYou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxSend = new System.Windows.Forms.RichTextBox();
+            this.SendMessage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(3, 75);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(471, 234);
+            this.txtLog.TabIndex = 28;
+            this.txtLog.Text = "";
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // richTextBoxSend
+            // 
+            this.richTextBoxSend.Location = new System.Drawing.Point(3, 322);
+            this.richTextBoxSend.Name = "richTextBoxSend";
+            this.richTextBoxSend.Size = new System.Drawing.Size(471, 92);
+            this.richTextBoxSend.TabIndex = 34;
+            this.richTextBoxSend.Text = "";
+            this.richTextBoxSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBoxSend_KeyDown);
+            // 
+            // SendMessage
+            // 
+            this.SendMessage.Location = new System.Drawing.Point(480, 322);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(75, 37);
+            this.SendMessage.TabIndex = 35;
+            this.SendMessage.Text = "发送";
+            this.SendMessage.UseVisualStyleBackColor = true;
+            this.SendMessage.Click += new System.EventHandler(this.SendMessage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "IP地址:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "端口号:";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(211, 2);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(151, 20);
+            this.textBoxIP.TabIndex = 38;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(213, 48);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(151, 20);
+            this.textBoxPort.TabIndex = 39;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(505, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 33);
+            this.buttonStart.TabIndex = 40;
+            this.buttonStart.Text = "启动服务";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(480, 377);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 37);
+            this.buttonBack.TabIndex = 41;
+            this.buttonBack.Text = "撤销";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(52, 22);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(60, 23);
+            this.textBoxName.TabIndex = 42;
+            this.textBoxName.Text = "客户端";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "昵称:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 426);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.richTextBoxSend);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.RichTextBox richTextBoxSend;
+        private System.Windows.Forms.Button SendMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TalkToYou/TalkToYou/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TalkToYou
+{
+    public partial class Form1 : Form
+    {
+        private static int port = 5200;
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            //关闭对文本框的非法线程操作检查
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            //string strlP = Dns.GetHostEntry("1w616580c9.iok.la").AddressList[0].ToString();
+            //textBoxIP.Text = strlP;
+            textBoxPort.Text = port.ToString();
+        }
+        //创建 1个客户端套接字 和1个负责监听服务端请求的线程
+        Thread threadclient = null;
+        Socket socketclient = null;
+        
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //SocketException exception;
+                this.buttonStart.Enabled = false;
+                //定义一个套接字监听
+                socketclient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //获取文本框中的IP地址
+
+                IPAddress address = IPAddress.Parse(textBoxIP.Text.Trim());
+
+
+
+                //将获取的IP地址和端口号绑定在网络节点上
+                IPEndPoint point = new IPEndPoint(address, int.Parse(textBoxPort.Text.Trim()));
+
+
+
+                try
+                {
+                    //客户端套接字连接到网络节点上，用的是Connect
+                    socketclient.Connect(point);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("连接失败\r\n");
+                    this.buttonStart.Enabled = true;
+                    return;
+                }
+            }
+            catch
+            {
+                
+                MessageBox.Show("请重新确认IP/端口格式");
+                this.buttonStart.Enabled = true;
+                return;
+            }
+            byte[] name = Encoding.UTF8.GetBytes(textBoxName.Text);
+            //调用客户端套接字发送字节数组   
+            socketclient.Send(name);
+
+            threadclient = new Thread(recv);
+
+            threadclient.IsBackground = true;
+
+            threadclient.Start(socketclient);
+           
+        }
+        // 接收服务端发来信息的方法  
+        private void recv(object socketserverpara)//
+        {
+            
+            Socket socketServer = socketserverpara as Socket;
+            while (true)//持续监听服务端发来的消息
+            {
+                try
+                {
+                    
+                    //定义一个1M的内存缓冲区，用于临时性存储接收到的消息
+                    byte[] arrRecvmsg = new byte[1024 * 1024];
+
+                    //将客户端套接字接收到的数据存入内存缓冲区，并获取长度
+                    int length = socketclient.Receive(arrRecvmsg);
+
+                    //将套接字获取到的字符数组转换为人可以看懂的字符串
+                    string strRevMsg = Encoding.UTF8.GetString(arrRecvmsg, 0, length);
+
+                    string []s=strRevMsg.Split('，');
+                                       
+                     if (s[0] != textBoxName.Text)
+                     {
+                         if (strRevMsg == "")
+                         {
+                             socketServer.Close();
+                             txtLog.AppendText(GetCurrentTime() + "\r\n" + "远程服务器已经中断连接" + "\r\n\n");
+                             this.buttonStart.Enabled = true;
+                             break;
+                         }
+                        txtLog.AppendText(GetCurrentTime() + "\r\n" + strRevMsg + "\r\n\n");
+                        
+                     }
+                  
+                }
+                catch (Exception ex)
+                {                  
+                    txtLog.AppendText("远程服务器已经中断连接" + "\r\n\n");
+                    this.buttonStart.Enabled = true;                    
+                    break;
+                }
+            }
+        }
+
+        //获取当前系统时间
+        private DateTime GetCurrentTime()
+        {
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            return currentTime;
+        }
+
+        //发送字符信息到服务端的方法
+        private void ClientSendMsg(string sendMsg)
+        {
+            //将输入的内容字符串转换为机器可以识别的字节数组   
+            byte[] arrClientSendMsg = Encoding.UTF8.GetBytes(textBoxName.Text+':'+sendMsg);
+            //调用客户端套接字发送字节数组   
+            socketclient.Send(arrClientSendMsg);
+            //将发送的信息追加到聊天内容文本框中   
+            txtLog.AppendText(GetCurrentTime() + "\r\n" + textBoxName.Text + ":" + sendMsg + "\r\n\n");
+        }
+
+        private void SendMessage_Click(object sender, EventArgs e)
+        {
+            //调用ClientSendMsg方法 将文本框中输入的信息发送给服务端   
+            ClientSendMsg(richTextBoxSend.Text.Trim());
+            richTextBoxSend.Clear();
+        }
+
+        private void richTextBoxSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            //当光标位于文本框时 如果用户按下了键盘上的Enter键
+            if (e.KeyCode == Keys.Enter)
+            {
+                //则调用客户端向服务端发送信息的方法  
+                ClientSendMsg(richTextBoxSend.Text.Trim());
+                richTextBoxSend.Clear();
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("是否退出？选否,最小化到托盘", "操作提示", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                
+                this.Dispose();
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+
+            }
+            else
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.Visible = false;
+                this.notifyIcon1.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            base.Visible = true;
+            this.notifyIcon1.Visible = false;
+            this.ShowInTaskbar = true;          
+            base.WindowState = FormWindowState.Normal;
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            richTextBoxSend.Clear();
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+            txtLog.SelectionStart = txtLog.Text.Length;
+            txtLog.ScrollToCaret(); 
+        }
+
+       
+
+    }
+}
